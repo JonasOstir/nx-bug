6f655640a85e3856421b92b3bbbc6d7665379a0e
Jonas Ostir <Jonas.Ostir@lightspeedhq.com>
2020-08-28 09:43:57 +0200

    Added emotion theming

--- apps/foo/pages/index.tsx
+++ apps/foo/pages/index.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import { ThemeProvider } from 'emotion-theming';
+
+const theme = {
+  colors: {
+    primary: 'hotpink',
+  },
+};
 
 export const Index = () => {
   return (
     <div>
-      <p>Welcome!</p>
+      <ThemeProvider theme={theme}>
+        <p css={(theme) => ({ color: theme.colors.primary })}>Welcome!</p>
+      </ThemeProvider>
     </div>
   );
 };

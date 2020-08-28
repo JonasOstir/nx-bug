529548669fa588bc0b4e642e3390eef430552a8a
Jonas Ostir <Jonas.Ostir@lightspeedhq.com>
2020-08-28 09:53:36 +0200

    Added @styled-system/css

--- apps/foo/pages/index.tsx
+++ apps/foo/pages/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { ThemeProvider } from 'emotion-theming';
+import { css } from '@styled-system/css';
+
 import Box from '../components/box';
 
 const theme = {
@@ -14,6 +16,7 @@ export const Index = () => {
       <ThemeProvider theme={theme}>
         <p css={(theme) => ({ color: theme.colors.primary })}>Welcome!</p>
         <Box p={2}>Styled System</Box>
+        <p css={css({ color: 'primary', m: 2 })}>@styled-system/css</p>
       </ThemeProvider>
     </div>
   );
